bfd3101cdd79d8f26b90e4b72683d23003e48e49
Eric Crosson <eric.s.crosson@utexas.edu>
2019-11-09 19:46:59 -0600

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import * as Future from 'fluture'
+
+declare function retry(time: (tries: number) => number): (max: number) => <A, B>(task: Future.FutureInstance<A, B>) => Future.FutureInstance<A[], B>;
+declare function exponentially(t: number): (n: number) => number;
+declare function linearly(t: number): (n: number) => number;
+declare function statically(t: number): (_: any) => number;
+declare function linearSeconds(n: number): number;
+declare function retryLinearly<A, B>(task: Future.FutureInstance<A, B>): Future.FutureInstance<A, B>;
